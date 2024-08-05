f256f59647249c29cfb596ef5b5272ee4ab32bc7
Roman <108451187+fremovro@users.noreply.github.com>
2024-08-05 13:09:11 +0500

    Add files via upload

---
+++ kisgis2api.js
@@ -0,0 +1,16 @@
+class kisgis2api {
+
+    constructor(window) { 
+        this._window = window;
+    }
+
+    showTreeNode(_id) {
+        let message = {
+            name: 'showTreeNode',
+            params: {
+                id: _id
+            }
+        }
+        this._window.postMessage(message, '*');
+    }
+  }
