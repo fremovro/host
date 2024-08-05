8e699b5a17b742fc473cd0c45c3c982ada7821c1
Roman <108451187+fremovro@users.noreply.github.com>
2024-08-05 17:45:33 +0500

    Update kisgis2api.js

--- kisgis2api.js
+++ kisgis2api.js
@@ -7,7 +7,15 @@ class kisgis2api {
     showTreeNode(_id) {
         let message = {
             name: 'showTreeNode',
-            params: [_id, 10]
+            params: [_id]
+        }
+        this._window.postMessage(message, '*');
+    }
+
+    complexExample(string1, string2, number1, number2, object1) {
+        let message = {
+            name: 'complexExample',
+            params: [string1, string2, number1, number2, object1]
         }
         this._window.postMessage(message, '*');
     }
